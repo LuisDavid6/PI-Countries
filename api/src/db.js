65429fb3fd8575b8cae6ad28e9e4ba924623b55d
David <luisdavid.0698@hotmail.com>
2022-11-21 10:58:38 -0500

    dv6

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize, Op} = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, URL
 } = process.env;
 
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
@@ -11,61 +11,106 @@ const {
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 // });
 
-let sequelize = process.env.NODE_ENV === "production"
-  ? new Sequelize({
-    database: DB_NAME,
-    dialect: "postgres",
-    host: DB_HOST,
-    port: 5432,
-    username: DB_USER,
-    password: DB_PASSWORD,
-    pool: {
-      max: 3,
-      min: 1,
-      idle: 10000,
+const sequelize = new Sequelize(URL,{
+  dialect:"postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-      keepAlive: true,
-    },
-    ssl: true,
-  })
-: new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    keepAlive: true,
+  },
+  ssl: true,
 });
 
-const basename = path.basename(__filename);
+// let sequelize = process.env.NODE_ENV === "production"
+//   ? new Sequelize({
+//     database: DB_NAME,
+//     dialect: "postgres",
+//     host: DB_HOST,
+//     port: 5432,
+//     username: DB_USER,
+//     password: DB_PASSWORD,
+//     pool: {
+//       max: 3,
+//       min: 1,
+//       idle: 10000,
+//     },
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false,
+//       },
+//       keepAlive: true,
+//     },
+//     ssl: true,
+//   })
+// : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
 
-const modelDefiners = [];
-
-// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/models'))
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
-  .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
-  });
-
-// Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach(model => model(sequelize));
-// Capitalizamos los nombres de los modelos ie: product => Product
-let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
-sequelize.models = Object.fromEntries(capsEntries);
-
-// En sequelize.models están todos los modelos importados como propiedades
-// Para relacionarlos hacemos un destructuring
-const { Activity, Country } = sequelize.models;
 
 
-// Aca vendrian las relaciones
-// Product.hasMany(Reviews);
-Country.belongsToMany(Activity, {through: "country_activities"})
-Activity.belongsToMany(Country, {through: "country_activities"})
+// let sequelize = process.env.NODE_ENV === "production"
+//   ? new Sequelize({
+//     database: DB_NAME,
+//     dialect: "postgres",
+//     host: DB_HOST,
+//     port: 5432,
+//     username: DB_USER,
+//     password: DB_PASSWORD,
+//     pool: {
+//       max: 3,
+//       min: 1,
+//       idle: 10000,
+//     },
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false,
+//       },
+//       keepAlive: true,
+//     },
+//     ssl: true,
+//   })
+// : new Sequelize(URL, {
+//   // logging: false, // set to console.log to see the raw SQL queries
+//   // native: false, // lets Sequelize know we can use pg-native for ~30% more speed,
+//   dialect:"postgres",
+// });
 
+
+  sequelize.authenticate().then( () => console.log('Connection has been established successfully.'))
+  .catch(error => console.error('Unable to connect to the database:', error))
+
+// const basename = path.basename(__filename);
+
+// const modelDefiners = [];
+
+// // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
+// fs.readdirSync(path.join(__dirname, '/models'))
+//   .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+//   .forEach((file) => {
+//     modelDefiners.push(require(path.join(__dirname, '/models', file)));
+//   });
+
+// // Injectamos la conexion (sequelize) a todos los modelos
+// modelDefiners.forEach(model => model(sequelize));
+// // Capitalizamos los nombres de los modelos ie: product => Product
+// let entries = Object.entries(sequelize.models);
+// let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+// sequelize.models = Object.fromEntries(capsEntries);
+
+// // En sequelize.models están todos los modelos importados como propiedades
+// // Para relacionarlos hacemos un destructuring
+// const { Activity, Country } = sequelize.models;
+
+
+// // Aca vendrian las relaciones
+// // Product.hasMany(Reviews);
+// Country.belongsToMany(Activity, {through: "country_activities"})
+// Activity.belongsToMany(Country, {through: "country_activities"})
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
