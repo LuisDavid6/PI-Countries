d38ddb71af98b458609d8b784c678b9d4f6092a9
David <luisdavid.0698@hotmail.com>
2023-12-08 00:40:23 -0500

    feat: change country details style

--- client/src/components/Country.jsx
+++ client/src/components/Country.jsx
@@ -1,56 +1,75 @@
-import style from "./Styles/Country.module.css"
-import { useDispatch, useSelector } from "react-redux"
-import { getCountry} from "../redux/actions"
-import React, { useEffect } from "react"
-import Activity from "./Activity"
-import {Link} from "react-router-dom"
+import style from './Styles/Country.module.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { getCountry } from '../redux/actions'
+import React, { useEffect } from 'react'
+import Activity from './Activity'
 
+export default function Country(props) {
+  const {
+    match: {
+      params: { id },
+    },
+  } = props
+  const dispatch = useDispatch()
+  const country = useSelector((state) => state.country)
+  const filter = useSelector((state) => state.filter)
 
-export default function Country(props){
-    const {match:{params:{id}}} = props
-    const dispatch = useDispatch()
-    const country = useSelector(state => state.country)
-    const filter = useSelector(state => state.filter)
+  useEffect(() => {
+    dispatch(getCountry(id))
+  }, [])
 
-    useEffect( ()=>{
-        dispatch(getCountry(id))
-    }, [])
+  useEffect(() => {}, [filter])
 
-    useEffect( ()=>{
-        
-    }, [filter])
-
-    return(
-        <div className={style.main}>
-                <button className={style.buttonBack} onClick={()=>props.history.goBack()}>Back</button>
-            <div className={style.container}>
-                <fieldset className={style.fieldset}>
-                    <legend className={style.legend}>Details</legend>
-                    <div className={style.details}>
-                        <div className={style.left}>
-                            <h1>{country && country.name}</h1>
-                            <img src={country.flagImage} className={style.img}></img>
-                        </div>
-                        <div className={style.rigth}>
-                            <h4>Code: {country.id}</h4>
-                            <h4>Capital: {country.capital}</h4>
-                            <h4>Continent: {country.continent}</h4>
-                            <h4>Subregion: {country.subregion}</h4>
-                            <h4>Population: {country.population}</h4>
-                            <h4>Area: {`${country.population} Km²`}</h4>
-                        </div>
-                    </div>
-                </fieldset>
-                <fieldset className={style.fieldset}>
-                    <legend className={style.legend}>Activities</legend>
-                    <div className={style.activities}>
-                        {country.activities && country.activities.length>0 ? country.activities.map(e=>{
-                            return <Activity data={e} key={e.name}/>
-                        }) : <h3>Not Activities Found</h3> }
-                        {/* <Link to="/createActivity"> <button className={style.add}>+</button> </Link> */}
-                    </div>
-                </fieldset>
-            </div>
+  return (
+    <div className={style.container}>
+      <button className={style.buttonBack} onClick={() => props.history.goBack()}>
+        Back
+      </button>
+      <fieldset className={style.fieldset}>
+        <legend className={style.legend}>Details</legend>
+        <div className={style.details}>
+          <div>
+            <h1>{country && country.name?.toUpperCase()}</h1>
+            <img src={country.flagImage} className={style.img}></img>
+          </div>
+          <div className={style.rigth}>
+            <section className={style.section}>
+              <h4>
+                Code: <span className={style.span}>{country.id}</span>
+              </h4>
+              <h4>
+                Capital: <span className={style.span}>{country.capital} </span>
+              </h4>
+              <h4>
+                Continent: <span className={style.span}>{country.continent}</span>
+              </h4>
+            </section>
+            <section className={style.section}>
+              <h4>
+                Subregion: <span className={style.span}>{country.subregion}</span>
+              </h4>
+              <h4>
+                Population: <span className={style.span}>{country.population}</span>
+              </h4>
+              <h4>
+                Area: <span className={style.span}>{`${country.population} Km²`}</span>
+              </h4>
+            </section>
+          </div>
         </div>
-    )
+      </fieldset>
+      <fieldset className={style.fieldset}>
+        <legend className={style.legend}>Activities</legend>
+        <div className={style.activities}>
+          {country.activities && country.activities.length > 0 ? (
+            country.activities.map((e) => {
+              return <Activity data={e} key={e.name} />
+            })
+          ) : (
+            <h3>Not Activities Found</h3>
+          )}
+        </div>
+      </fieldset>
+    </div>
+  )
 }
